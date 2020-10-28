fa3e8aa2aaf1f128b09313a2b90400a0b0037a3b
Simpliii <rory.dwy@gmail.com>
2020-10-27 21:46:44 -0700

    change server line again 4

--- server/server.js
+++ server/server.js
@@ -66,4 +66,6 @@ io.on('connection', (sock) => {
 });
 
 // Start server and log errors to console
-server.listen(process.env.PORT, '0.0.0.0')
+server.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
